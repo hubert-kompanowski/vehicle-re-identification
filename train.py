03bf685d836a080b35951aadf0647237254f94d4
Hubert Kompanowski <hubert.kompanowski9@gmail.com>
2022-07-09 16:33:39 +0200

    template

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import dotenv
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 # load environment variables from `.env` file if it exists
 # recursively searches for `.env` in all folders starting from work dir
@@ -9,6 +9,7 @@ dotenv.load_dotenv(override=True)
 
 @hydra.main(config_path="configs/", config_name="train.yaml")
 def main(config: DictConfig):
+    print(OmegaConf.to_yaml(config))
 
     # Imports can be nested inside @hydra.main to optimize tab completion
     # https://github.com/facebookresearch/hydra/issues/934
