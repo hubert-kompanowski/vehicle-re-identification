5680cb1cbb858316a407310036fe99a26900b3f0
Hubert Kompanowski <hubert.kompanowski9@gmail.com>
2022-07-12 18:00:54 +0200

    to train model

--- src/datamodules/city_flow_datamodule.py
+++ src/datamodules/city_flow_datamodule.py
@@ -1,37 +1,60 @@
+import random
 import xml.etree.ElementTree as ET
 from pathlib import Path
 from typing import Optional, Tuple
 
+import hydra
 import pandas as pd
 import torch
 from hydra.utils import to_absolute_path
 from pytorch_lightning import LightningDataModule
+from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset, random_split
 from torchvision.io import read_image
 from torchvision.transforms import transforms
 
 
 class CityFlowDataset(Dataset):
-    def __init__(self, df, transform=None):
+    def __init__(self, df, all_ids, test=False, transform=None):
         self.df = df
+        self.all_ids = all_ids
         self.transform = transform
+        self.test = test
 
     def __len__(self):
-        return len(self.df)
+        if self.test:
+            return len(self.df)
+        return len(self.all_ids)
 
     def __getitem__(self, item):
-        image_path, vehicle_id = self.df.iloc[item]
-        image = read_image(image_path)
-        if self.transform:
-            image = self.transform(image)
-        return image, vehicle_id
+        if self.test:
+            image_path, vid = self.df.iloc[item]
+            image = torch.tensor(read_image(image_path), dtype=torch.float)
+            return image, vid
+
+        anchor_vehicle_id = self.all_ids[item]
+
+        data = self.df[self.df["vehicle_id"] == anchor_vehicle_id].sample(n=2)
+
+        anchor_image_path, v1 = data.iloc[0]
+        positive_image_path, v2 = data.iloc[1]
+        negative_image_path, v3 = (
+            self.df[self.df["vehicle_id"] != anchor_vehicle_id].sample(n=1).iloc[0]
+        )
+
+        results = []
+        for image_path in [anchor_image_path, positive_image_path, negative_image_path]:
+            image = torch.tensor(read_image(image_path), dtype=torch.float)
+            if self.transform:
+                image = self.transform(image)
+            results.append(image)
+        return results, 1
 
 
 class CityFlowDataModule(LightningDataModule):
     def __init__(
         self,
         data_dir: str = "data/",
-        train_val_test_split: Tuple[int, int, int] = (40000, 4000, 8000),
         batch_size: int = 64,
         num_workers: int = 0,
         pin_memory: bool = False,
@@ -43,7 +66,6 @@ class CityFlowDataModule(LightningDataModule):
         self.transforms = transforms.Compose(
             [
                 transforms.Resize(size=(256, 256)),
-                transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
             ]
         )
@@ -68,11 +90,23 @@ class CityFlowDataModule(LightningDataModule):
 
     def setup(self, stage: Optional[str] = None):
         if not self.data_train and not self.data_val and not self.data_test:
-            train_set = CityFlowDataset(self.hparams.data_dir, transform=self.transforms)
-            self.data_train, self.data_val, self.data_test = random_split(
-                dataset=train_set,
-                lengths=self.hparams.train_val_test_split,
-                generator=torch.Generator().manual_seed(42),
+            train_ids, test_ids = train_test_split(self.all_ids, test_size=0.3, random_state=42)
+            val_ids, test_ids = train_test_split(test_ids, test_size=0.5, random_state=42)
+
+            self.data_train = CityFlowDataset(
+                self.df[self.df["vehicle_id"].isin(train_ids)],
+                train_ids,
+                test=False,
+                transform=self.transforms,
+            )
+            self.data_val = CityFlowDataset(
+                self.df[self.df["vehicle_id"].isin(val_ids)],
+                val_ids,
+                test=False,
+                transform=self.transforms,
+            )
+            self.data_test = CityFlowDataset(
+                self.df[self.df["vehicle_id"].isin(test_ids)], test_ids, test=False
             )
 
     def train_dataloader(self):
--- src/models/simple_reid_module.py
+++ src/models/simple_reid_module.py
@@ -1,3 +1,4 @@
+import pickle
 from typing import Any, List
 
 import torch
@@ -18,78 +19,90 @@ class SimpleReIdLitModule(LightningModule):
 
         # use separate metric instance for train, val and test step
         # to ensure a proper reduction over the epoch
-        self.train_acc = LabelRankingAveragePrecision()
-        self.val_acc = LabelRankingAveragePrecision()
-        self.test_acc = LabelRankingAveragePrecision()
-
-        # for logging best so far validation accuracy
-        self.val_acc_best = MaxMetric()
+        # self.train_acc = LabelRankingAveragePrecision()
+        # self.val_acc = LabelRankingAveragePrecision()
+        # self.test_acc = LabelRankingAveragePrecision()
+        #
+        # # for logging best so far validation accuracy
+        # self.val_acc_best = MaxMetric()
 
     def forward(self, x: torch.Tensor):
         return self.net(x)
 
-    def on_train_start(self):
-        # by default lightning executes validation step sanity checks before training starts,
-        # so we need to make sure val_acc_best doesn't store accuracy from these checks
-        self.val_acc_best.reset()
+    # def on_train_start(self):
+    #     # by default lightning executes validation step sanity checks before training starts,
+    #     # so we need to make sure val_acc_best doesn't store accuracy from these checks
+    #     self.val_acc_best.reset()
 
     def step(self, batch: Any):
+        x, _ = batch
+        anchor_logits = self.forward(x[0])
+        positive_logits = self.forward(x[1])
+        negative_logits = self.forward(x[2])
+        loss = self.criterion(anchor_logits, positive_logits, negative_logits)
+
+        return loss
+
+    def embed(self, batch: Any):
         x, y = batch
-        logits = self.forward(x)
-        loss = self.criterion(logits, y)
-        preds = torch.argmax(logits, dim=1)
-        return loss, preds, y
+        embeddings = self.forward(x)
+        return embeddings, y
 
     def training_step(self, batch: Any, batch_idx: int):
-        loss, preds, targets = self.step(batch)
+        loss = self.step(batch)
 
         # log train metrics
-        acc = self.train_acc(preds, targets)
+        # acc = self.train_acc(preds, targets)
         self.log("train/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
-        self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        # self.log("train/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
 
         # we can return here dict with any tensors
         # and then read it in some callback or in `training_epoch_end()` below
         # remember to always return loss from `training_step()` or else backpropagation will fail!
-        return {"loss": loss, "preds": preds, "targets": targets}
+        return {"loss": loss}  # , "preds": preds, "targets": targets}
 
     def training_epoch_end(self, outputs: List[Any]):
         # `outputs` is a list of dicts returned from `training_step()`
         pass
 
     def validation_step(self, batch: Any, batch_idx: int):
-        loss, preds, targets = self.step(batch)
+        loss = self.step(batch)
 
         # log val metrics
-        acc = self.val_acc(preds, targets)
+        # acc = self.val_acc(preds, targets)
         self.log("val/loss", loss, on_step=False, on_epoch=True, prog_bar=False)
-        self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
+        # self.log("val/acc", acc, on_step=False, on_epoch=True, prog_bar=True)
 
-        return {"loss": loss, "preds": preds, "targets": targets}
+        return {"loss": loss}  # , "preds": preds, "targets": targets}
 
     def validation_epoch_end(self, outputs: List[Any]):
-        acc = self.val_acc.compute()  # get val accuracy from current epoch
-        self.val_acc_best.update(acc)
-        self.log("val/acc_best", self.val_acc_best.compute(), on_epoch=True, prog_bar=True)
+        pass
+        # acc = self.val_acc.compute()  # get val accuracy from current epoch
+        # self.val_acc_best.update(acc)
+        # self.log("val/acc_best", self.val_acc_best.compute(), on_epoch=True, prog_bar=True)
 
     def test_step(self, batch: Any, batch_idx: int):
-        loss, preds, targets = self.step(batch)
-
-        # log test metrics
-        acc = self.test_acc(preds, targets)
-        self.log("test/loss", loss, on_step=False, on_epoch=True)
-        self.log("test/acc", acc, on_step=False, on_epoch=True)
-
-        return {"loss": loss, "preds": preds, "targets": targets}
+        embedding, vehicle_id = self.embed(batch)
+        return embedding, vehicle_id
+        # return {"embedding": embedding, "vehicle_id": vehicle_id}
 
     def test_epoch_end(self, outputs: List[Any]):
-        pass
+        with open("test_output.pkl", "wb") as file:
+            pickle.dump(outputs, file)
+            print("test outputs saved")
 
-    def on_epoch_end(self):
-        # reset metrics at the end of every epoch
-        self.train_acc.reset()
-        self.test_acc.reset()
-        self.val_acc.reset()
+        # embeddings = [element[0] for sublist in outputs for element in sublist]
+        # vehicle_ids = [element[1] for sublist in outputs for element in sublist]
+        # distances = torch.cdist(torch.tensor(embeddings), torch.tensor(embeddings))
+        # for vid, dists in zip(vehicle_ids, distances):
+        #
+        # pass
+
+    # def on_epoch_end(self):
+    #     # reset metrics at the end of every epoch
+    #     self.train_acc.reset()
+    #     self.test_acc.reset()
+    #     self.val_acc.reset()
 
     def configure_optimizers(self):
         """Choose what optimizers and learning-rate schedulers to use in your optimization.
